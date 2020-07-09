49b6a2d82a5c34115bbab8262ff2b4dae36da81e
ankit <ankit@ankit-PC>
2020-07-09 13:26:45 +0530

    First commit of hello world java project.

---
+++ hellojava/src/com/hellojava/main/MainClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.hellojava.main;
+
+/**
+ * @author ankit
+ *
+ */
+public class MainClass {
+
+	/**
+	 * 
+	 */
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+
+	}
+
+}
